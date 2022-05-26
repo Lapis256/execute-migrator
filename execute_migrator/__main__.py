d77d91bdaebb0628fe8529421b5db391075b132a
Lapis <45414333+Lapis256@users.noreply.github.com>
2022-05-27 00:19:44 +0900

    setup

---
+++ execute_migrator/__main__.py
@@ -0,0 +1,3 @@
+def main():
+    print("Execute migrator")
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="execute-migrator",
+    version="1.0.0",
+    install_requires=[
+        "amulet-nbt>=1.2.0,<1.3.0"
+    ],
+    extras_require={
+        ":sys_platform == 'linux' and platform_machine == 'aarch64'": [
+            "bedrock@git+https://github.com/Lapis256/bedrock@master"
+        ],
+        ":platform_machine != 'aarch64'": [
+            "bedrock@git+https://github.com/BluCodeGH/bedrock@master"
+        ]
+    },
+    packages=["execute_migrator"],
+    entry_points={
+        "console_scripts": [
+            "execute_migrator = execute_migrator.__main__:main"
+        ]
+    }
+)
