1662989f7bcb0b8dcb3da49458c45089899514d1
Lapis <45414333+Lapis256@users.noreply.github.com>
2022-05-28 18:17:49 +0900

    add command

--- execute_migrator/__main__.py
+++ execute_migrator/__main__.py
@@ -1,3 +1,64 @@
-def main():
-    print("Execute migrator")
+from __future__ import annotations
 
+from argparse import ArgumentParser, Namespace, _SubParsersAction
+
+from mcfunction import parse_command
+
+from mcfunction.versions.mcbe_1_19.execute import ParsedExecuteCommand
+
+EXECUTE_TEMPLATE_BASE = "execute as {0.target} at @s positioned {0.position} "
+EXECUTE_TEMPLATE = EXECUTE_TEMPLATE_BASE + "run {run}"
+EXECUTE_DETECT_TEMPLATE = (
+    EXECUTE_TEMPLATE_BASE + "if block {0.detect_position} {0.block} {0.data} run {run}"
+)
+
+
+def migrate_execute(command_text: str) -> str:
+    command: ParsedExecuteCommand = parse_command(command_text)
+
+    run: str = command.run
+    if run.startswith(("/execute", "execute")):
+        run = migrate_execute(run.lstrip("/"))
+
+    if command.detect is None:
+        return EXECUTE_TEMPLATE.format(command, run=run)
+
+    return EXECUTE_DETECT_TEMPLATE.format(command, run=run)
+
+
+def default(parser: ArgumentParser, _):
+    parser.print_help()
+
+
+def migrate_text(parser: ArgumentParser, args: Namespace):
+    print(migrate_execute(args.command))
+
+
+def add_migrate_text(subparser: _SubParsersAction[ArgumentParser]):
+    parser = subparser.add_parser("text", help="指定した execute コマンドを移行する")
+    parser.set_defaults(func=migrate_text)
+    parser.add_argument("command", help="移行するコマンド")
+
+
+def migrate_mcfunction(parser: ArgumentParser, args: Namespace):
+    pass
+
+
+def add_migrate_mcfunction(subparser: _SubParsersAction[ArgumentParser]):
+    parser = subparser.add_parser(
+        "mcfunction", help=".mcfunction ファイル内の execute コマンドを移行する[WIP]"
+    )
+    parser.set_defaults(func=migrate_mcfunction)
+    parser.add_argument("path", help=".mcfunction ファイル")
+
+
+def main():
+    parser = ArgumentParser(description="migrator")
+    parser.set_defaults(func=default)
+
+    subparser = parser.add_subparsers()
+    add_migrate_text(subparser)
+    add_migrate_mcfunction(subparser)
+
+    args = parser.parse_args()
+    args.func(parser, args)
--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="execute-migrator",
     version="1.0.0",
     install_requires=[
-        "amulet-nbt>=1.2.0,<1.3.0"
+        "amulet-nbt>=1.2.0,<1.3.0",
+        "mcfunction-be.py@git+https://github.com/Lapis256/mcfunction-be.py.git",
     ],
     extras_require={
         ":sys_platform == 'linux' and platform_machine == 'aarch64'": [
@@ -12,12 +13,10 @@ setup(
         ],
         ":platform_machine != 'aarch64'": [
             "bedrock@git+https://github.com/BluCodeGH/bedrock@master"
-        ]
+        ],
     },
     packages=["execute_migrator"],
     entry_points={
-        "console_scripts": [
-            "execute_migrator = execute_migrator.__main__:main"
-        ]
-    }
+        "console_scripts": ["execute_migrator = execute_migrator.__main__:main"]
+    },
 )
